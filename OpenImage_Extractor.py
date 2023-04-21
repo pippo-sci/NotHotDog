9db04acbfe211a28d065cba08cd46a09fea729c5
Felipe Ramos <framosmorale@student.unimelb.edu.au>
2023-04-21 18:59:39 +1000

    datadowload

--- OpenImage_Extractor.py
+++ OpenImage_Extractor.py
@@ -1,9 +1,10 @@
 import fiftyone as fo
-
+fo.config.dataset_zoo_dir = "./Dataset"
 fo.zoo.load_zoo_dataset(
               "open-images-v7",
               split="train",
               label_types=["classifications"],
-              classes=["Taco", "Hot dog"],
-              max_samples=100,
+              classes=["Dog","Taco","Hot dog"],
+              shuffle=True,
+              max_samples=3000,
           )
