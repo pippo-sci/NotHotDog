c17e1cf16204d5d4a70b8a201398b1e6ba553966
Felipe Ramos <framosmorale@student.unimelb.edu.au>
2023-04-25 22:43:46 +1000

    working_pipeline

--- processing_pipeline.py
+++ processing_pipeline.py
@@ -11,23 +11,24 @@ class preprocess:
     
     def load_data_img(self, rootpath):
         
-        for i, filename in enumerate(os.listdir(os.path.join(rootpath, 'train/'))):
+        for i, filename in enumerate(os.listdir(os.path.join(rootpath, 'data'))):
 
-            img = cv2.imread(os.path.join(rootpath, f'train/{filename}'),cv2.IMREAD_GRAYSCALE)
+            img = cv2.imread(os.path.join(rootpath, f'data\{filename}'),cv2.IMREAD_GRAYSCALE)
             
             file_name_no_ext=filename.replace('.jpg','')
             self.img_name_list.append(file_name_no_ext)
-            
-            self.dataset.loc[file_name_no_ext,'img']=[img]
+            #self.dataset.loc[file_name_no_ext,'img']=[img]
+        print(len(self.img_name_list))
     
     def set_dataframe(self, rootpath):
 
-        cases = os.listdir('Dataset\open-images-v7\\train\data')
+        cases = os.listdir(rootpath + '\data')
         cases = [i.split('.')[0] for i in cases]
-        classi = pd.read_csv('Dataset\open-images-v7\\train\labels\classifications.csv')
-        class_map = pd.read_csv('Dataset\open-images-v7\\train\metadata\classes.csv', names=['Labelname', 'label'])
+        classi = pd.read_csv(rootpath + '\labels\classifications.csv')
+        class_map = pd.read_csv(rootpath + '\metadata\classes.csv', names=['Labelname', 'label'])
         df = classi[(classi.Confidence > 0) & classi.ImageID.isin(cases)].merge(class_map, how='left', left_on='LabelName', right_on='Labelname')
-        self.dataset = df.groupby('ImageID').label.unique()
+        ref = ['Hot dog', 'Dog', 'Taco']
+        self.dataset = df.groupby('ImageID', as_index=False).label.apply(lambda x: ' '.join([i for i in ref if i in list(x)]))
 
     def process_one(self):
         pass
@@ -36,13 +37,14 @@ class preprocess:
         pass
 
     def out_put(self, path):
-        pass
+        print(self.dataset.label.value_counts())
 
 if __name__ == "__main__":
     # Execute pipeline
+    path = "Dataset\open-images-v7\\train"
     current_process = preprocess()
-    current_process.set_dataframe("/")
-    current_process.load_data_img("/")
+    current_process.set_dataframe(path)
+    current_process.load_data_img(path)
     current_process.process_one()
     current_process.process_two()
     current_process.out_put("/")
