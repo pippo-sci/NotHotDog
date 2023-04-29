471f277638f5043b9148b27e4e75a70a07e26142
Eduardo Rossel <eduardorossel@outlook.cl>
2023-04-29 12:50:52 -0400

    Adding Django Website

---
+++ WebDjango/NotHotDog/NotHotDogWebsite/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+  return render(request, 'index.html', {}) 
+
+def about(request):
+  return render(request, 'about.html', {}) 
+
+def dashboard(request):
+  return render(request, 'dashboard.html', {}) 
