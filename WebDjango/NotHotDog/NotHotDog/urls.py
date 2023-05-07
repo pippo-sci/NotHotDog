c7158a31274e9b590c5ecaee1a61f17d7751cb5d
Eduardo Rossel <eduardorossel@outlook.cl>
2023-05-07 17:51:55 -0400

    Implementación de dropzone js

--- WebDjango/NotHotDog/NotHotDog/urls.py
+++ WebDjango/NotHotDog/NotHotDog/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from NotHotDogWebsite.views import index, file_upload_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('NotHotDogWebsite.urls')),
+    path('', include('NotHotDogWebsite.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- WebDjango/NotHotDog/NotHotDogWebsite/views.py
+++ WebDjango/NotHotDog/NotHotDogWebsite/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.http import HttpResponse, JsonResponse
+from .models import Doc
 
 # Create your views here.
 
 def index(request):
   return render(request, 'index.html', {}) 
+
+def file_upload_view(request):
+  print(request.FILES)
+  if request.method == 'POST':
+    my_file = request.FILES.get('file')
+    Doc.objects.create(upload = my_file)
+    return HttpResponse('')
+  return JsonResponse({'post':'false'})
 
 def about(request):
   return render(request, 'about.html', {}) 
