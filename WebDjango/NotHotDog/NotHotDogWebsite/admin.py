51fc3ca9f062cebaa0f5222bbb8f6373af7ae107
Eduardo Rossel <eduardorossel@outlook.cl>
2023-05-21 15:27:19 -0400

    Añadir faker, crear modelo de predicciones

--- WebDjango/NotHotDog/NotHotDogWebsite/admin.py
+++ WebDjango/NotHotDog/NotHotDogWebsite/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Doc
+from .models import Doc, Predictions
+
 
 # Register your models here.
 admin.site.register(Doc)
+admin.site.register(Predictions)
