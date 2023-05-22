782684a98a63859801677638edef4b463d64650d
Eduardo Rossel <eduardorossel@outlook.cl>
2023-05-21 21:26:29 -0400

    Modificaciones al dashboard - Primeros KPI

--- WebDjango/NotHotDog/NotHotDogWebsite/views.py
+++ WebDjango/NotHotDog/NotHotDogWebsite/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.http import HttpResponse, JsonResponse
-from .models import Doc
+from .models import Doc, Predictions
+from django.db import models
 
 # Create your views here.
 
@@ -20,7 +21,30 @@ def about(request):
   return render(request, 'about.html', {}) 
 
 def dashboard(request):
-  return render(request, 'dashboard.html', {}) 
+  # Get all predictions from the database
+  predictions = Predictions.objects.all()
+  
+  # Calculate accuracy
+  total_predictions = predictions.count()
+  total_conformity = predictions.filter(conformity=True).count()
+  accuracy = total_conformity / total_predictions * 100 if total_predictions > 0 else 0
+  accuracy_formatted = "{:.1f}".format(accuracy)
+  
+  # Calculate average time of response
+  total_response_time = predictions.aggregate(models.Sum('t_response'))['t_response__sum']
+  average_response_time = total_response_time / total_predictions if total_predictions > 0 else 0
+  average_response_time_formatted = "{:.1f}".format(average_response_time)
+
+  # Pass the data to the template
+  context = {
+      'predictions': predictions,
+      'accuracy': accuracy_formatted,
+      'average_response_time': average_response_time_formatted,
+      # Include any additional data in the context
+      # ...
+  }
+
+  return render(request, 'dashboard.html', context) 
 
 def footer(request):
   return render(request, 'footer.html', {}) 
