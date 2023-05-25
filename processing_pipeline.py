7e4cff9d9a499f01927f81c983d4480e5c79ab1e
Felipe Ramos <framosmorale@student.unimelb.edu.au>
2023-05-25 23:02:47 +1000

    pipeline export fix

--- processing_pipeline.py
+++ processing_pipeline.py
@@ -12,17 +12,6 @@ class preprocess:
         self.dataset = pd.DataFrame()
         self.img_name_list=[]
     
-    def load_data_img(self, rootpath):
-        logging.info(f'Loading Images from {rootpath} started...')
-        for i, filename in enumerate(os.listdir(os.path.join(rootpath, 'data'))):
-
-            img = cv2.imread(os.path.join(rootpath, f'data\{filename}'),cv2.IMREAD_GRAYSCALE)
-            img = cv2.resize(img, (600,600))
-            file_name_no_ext=filename.replace('.jpg','')
-            self.img_name_list.append(file_name_no_ext)
-            self.dataset.loc[file_name_no_ext,'img']=[img]
-
-        logging.info(f'{len(self.img_name_list)} Images loaded')
     
     def set_dataframe(self, rootpath):
         logging.info('Loading labels started...')
@@ -37,8 +26,26 @@ class preprocess:
         self.dataset = self.dataset.set_index('ImageID')
         logging.info("Dataset labels finished")
 
+
+    def load_data_img(self, rootpath):
+        logging.info(f'Loading Images from {rootpath} started...')
+        for i, filename in enumerate(os.listdir(os.path.join(rootpath, 'data'))):
+
+            img = cv2.imread(os.path.join(rootpath, f'data\{filename}'), cv2.IMREAD_GRAYSCALE)
+            img = cv2.resize(img, (600,600))
+            file_name_no_ext=filename.replace('.jpg','')
+            self.img_name_list.append(file_name_no_ext)
+            self.dataset.loc[file_name_no_ext,'img']=[img]
+
+        logging.info(f'{len(self.img_name_list)} Images loaded')
+
+
     def process_one(self):
+        """
+        Normalise img values
+        """
         logging.info('Preprocess one started...')
+
         self.dataset['img'] = self.dataset.img.apply(lambda x: x[0]/255)
         #.apply(lambda x: 'no' if x is None else print(x[0].shape))
         logging.info("Preprocess one finish")
@@ -47,17 +54,16 @@ class preprocess:
         logging.info("Data Augmentation started...")
         second_df = pd.DataFrame()
         
-        for id, (label, img) in enumerate(self.dataset.iterrows()):
-            flip_img = np.flipud(img)
-            pd.concat([second_df,{'ImagedID': str(id)+'r',
-                                 'label': label,
-                                 'img': flip_img}])
-            
-            turn_img = np.fliplr(img)
-            pd.concat([second_df,{'ImagedID':str(id)+'fr',
-                                 'label': label,
-                                 'img': turn_img}])
-        
+        for imgID, (label, img) in self.dataset.iterrows():
+            if label is not np.nan and "Hot dog" in label:
+                flip_img = np.flipud(img)
+                turn_img = np.fliplr(img)
+                augmented = {'ImageID': [str(imgID)+'flip', str(imgID)+'turn'],
+                            'label': [label]*2,
+                            'img': [flip_img,turn_img]}
+                
+                second_df = pd.concat([second_df,pd.DataFrame(augmented)])
+    
         second_df.set_index('ImageID', inplace=True)
 
         self.dataset = pd.concat([self.dataset, second_df])
@@ -66,8 +72,17 @@ class preprocess:
 
     def out_put(self, path):
         #print(self.dataset.label.value_counts())
-        self.dataset.to_csv('test.csv')
+        #self.dataset.to_csv('test.csv')
+        #self.dataset.to_pickle('test.pkl.gzip')
+        self.dataset.to_hdf('test.h5', mode='w', key='df')
+        try:
+            self.dataset.to_json('test.json')
+        except Exception as e:
+            logging.warning(e)
+            print('exception')
+
         logging.info(f'Data output in: {path}test.csv')
+
 
 if __name__ == "__main__":
     # Execute pipeline
@@ -77,4 +92,4 @@ if __name__ == "__main__":
     current_process.load_data_img(path)
     current_process.process_one()
     current_process.augmentation_process()
-    current_process.out_put("/")
+    current_process.out_put("\\")
